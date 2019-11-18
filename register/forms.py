b44aa8ed4445549525d2c8a33156ab320acf68f1
jpsoousa <jps.soousa@gmail.com>
2019-11-17 21:03:36 -0300

    fix

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,5 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
-from django.conf import settings
 
 
 class CustomUserCreationForm(UserCreationForm):
